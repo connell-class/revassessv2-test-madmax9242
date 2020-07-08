a3d8302f083548aea36c89c5b2c9c64ffa653d70
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-07-08 16:27:32 -0400

    testing only iterationSum

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,11 +3,12 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
-    	if(s1.equals(s2)) {
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
+//    	if(s1.equals(s2)) {
+//    		return true;
+//    	}
+//    	else {
+//    		return false;
+//    	}
+    	return false;
     }
 }
